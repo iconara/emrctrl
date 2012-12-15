f3a9e2e9a384920a31dc7c48160f0a6499719751
Theo <theo@iconara.net>
2012-12-15 19:32:46 +0100

    Moved flow loading and management to a service + selected flow

--- public/app/main.js
+++ public/app/main.js
@@ -43,51 +43,81 @@
 
 ;(function () {
   var module = angular.module("emrctrl", ["emrctrl.formatting"])
+
+  module.factory("flows", function ($http, $q, $log) {
+    var self = {}
+    var flows = []
+
+    var prepareFlow = function (flow) {
+      flow.elapsed_time = flow.started_at == 0 ? 0 : (flow.ended_at - flow.started_at) * 1000
+      flow.created_at = new Date(flow.created_at * 1000)
+      flow.ready_at = new Date(flow.ready_at * 1000)
+      flow.started_at = new Date(flow.started_at * 1000)
+      flow.ended_at = new Date(flow.ended_at * 1000)
+      return flow
+    }
+
+    var descendingCreationOrder = function (a, b) {
+      return b.created_at.getTime() - a.created_at.getTime()
+    }
+
+    var stateFilter = function (state) {
+      return function (flow) { return flow.state == state }
+    }
+
+    var loadFlowData = function (flow) {
+      var url = "/v1/flows/" + flow.job_flow_id
+      return $http.get(url).then(function (response) {
+        return prepareFlow(response.data)
+      }).then(function (loadedFlow) {
+        flows[flows.indexOf(flow)] = loadedFlow
+        return loadedFlow
+      })
+    }
+
+    self.loadFlows = function () {
+      var url = "/v1/flows"
+      return $http.get(url).then(function (response) {
+        flows = response.data
+        flows.forEach(prepareFlow)
+        flows.sort(descendingCreationOrder)
+        flows.filter(stateFilter("RUNNING")).forEach(loadFlowData)
+        return self
+      })
+    }
+
+    self.loadFlow = function (flow) {
+      return loadFlowData(flow)
+    }
+
+    self.flows = function () {
+      return flows
+    }
+
+    return self
+  })
+
+  module.controller("AppController", function ($scope, flows, $log) {
+    $scope.flows = []
+    $scope.selectedFlow = null
+    $scope.loading = false
+
+    $scope.reload = function () {
+      $scope.loading = true
+
+      flows.loadFlows().then(function () {
+        $scope.loading = false
+        $scope.flows = flows.flows()
+      })
+    }
+
+    $scope.selectFlow = function (flow) {
+      flows.loadFlow(flow).then(function (flow) {
+        $scope.flows = flows.flows()
+        $scope.selectedFlow = flow
+      })
+    }
+
+    $scope.reload()
+  })
 })()
-
-var AppController = function ($scope, $http, $q, $log) {
-  $scope.flows = []
-  $scope.loading = false
-
-  $scope.reload = function () {
-    $scope.loading = true
-    var request = $http.get("/v1/flows")
-    request.success(function (data) {
-      $scope.loading = false
-      $scope.flows = data
-      $scope.flows
-        .map(prepareFlow)
-        .sort(descendingCreationOrder)
-
-      $scope.flows
-        .filter(function (flow) { return flow.state == "RUNNING" })
-        .forEach(loadAdditionalData)
-    })
-    request.error(function(data, status, headers, config) {
-      $log.error("BLURGH")
-      $scope.loading = false
-    })
-  }
-
-  $scope.reload()
-
-  var loadAdditionalData = function (flow) {
-    var request = $http.get("/v1/flows/" + flow.job_flow_id)
-    request.success(function (data) {
-      $scope.flows[$scope.flows.indexOf(flow)] = prepareFlow(data)
-    })
-  }
-
-  var prepareFlow = function (flow) {
-    flow.elapsed_time = flow.started_at == 0 ? 0 : (flow.ended_at - flow.started_at) * 1000
-    flow.created_at = new Date(flow.created_at * 1000)
-    flow.ready_at = new Date(flow.ready_at * 1000)
-    flow.started_at = new Date(flow.started_at * 1000)
-    flow.ended_at = new Date(flow.ended_at * 1000)
-    return flow
-  }
-
-  var descendingCreationOrder = function (a, b) {
-    return b.created_at.getTime() - a.created_at.getTime()
-  }
-}
