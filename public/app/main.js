c65d39f9a65fd9dd54fbfea9dc20b487e3895b12
Theo <theo@iconara.net>
2012-12-17 06:58:53 +0100

    Pedantic quotes

--- public/app/main.js
+++ public/app/main.js
@@ -1,11 +1,11 @@
 ;(function () {
   moment.calendar = {
-    lastDay : '[Yesterday at] HH:mm',
-    sameDay : '[Today at] HH:mm',
-    nextDay : '[Tomorrow at] HH:mm',
-    lastWeek : '[last] dddd [at] HH:mm',
-    nextWeek : 'dddd [at] HH:mm',
-    sameElse : 'YYYY-MM-DD HH:mm'
+    lastDay : "[Yesterday at] HH:mm",
+    sameDay : "[Today at] HH:mm",
+    nextDay : "[Tomorrow at] HH:mm",
+    lastWeek : "[last] dddd [at] HH:mm",
+    nextWeek : "dddd [at] HH:mm",
+    sameElse : "YYYY-MM-DD HH:mm"
   }
 })()
 
